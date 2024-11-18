420c1aaadcfea4ac8f5dfacf4207d52685991f35
DanWooldridge <157598676+AstroCatEntertainment@users.noreply.github.com>
2024-11-18 11:52:24 +0000

    Fixing Repo and project

--- Coursework_Audio/AudioCoursework/Intermediate/Build/Win64/x64/AudioCourseworkEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h
+++ Coursework_Audio/AudioCoursework/Intermediate/Build/Win64/x64/AudioCourseworkEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "D:/AbertayWork/Audio/Coursework_Audio/AudioCoursework/Intermediate/Build/Win64/x64/AudioCourseworkEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
+#include "M:/GitHub/Audio/AudioSoundEffectInUnreal/Coursework_Audio/AudioCoursework/Intermediate/Build/Win64/x64/AudioCourseworkEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
